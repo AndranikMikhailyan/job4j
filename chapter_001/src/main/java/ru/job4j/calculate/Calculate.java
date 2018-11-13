d9f925cc0d4088128cef9d69b52c66196812464f
AndranikMikhailyan <andranik.stav@mail.ru>
2018-11-13 22:07:04 +0300

    1.3. JavaDoc. Code convention[#84438]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+* Calculate
+*
+* @author Andranik Mikhailyan
+*/
 public class Calculate {
-
+	/**
+	* Main, точка входа в программу
+	* Вывод текста
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
