bdfd96d08db87f7b1eeec3b05277e61cb4e97cb3
AndranikMikhailyan <andranik.stav@mail.ru>
2018-11-14 19:08:12 +0300

    ничего не изменил

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test
-*
-* @author Andranik Mikhailyan
-*/
+ * Test
+ *
+ * @author Andranik Mikhailyan
+ */
 public class CalculateTest {
 	/**
-	* Test echo.
-	*/
+	 * Test echo.
+	 */
 	@Test
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Andranik Mikhailyan";
-		String expect = "Echo, echo, echo : Andranik Mikhailyan"; 
+		String expect = "Echo, echo, echo : Andranik Mikhailyan";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
 	}
- 
+
 }
 
