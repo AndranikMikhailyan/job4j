4ee63b3414e3562380a111493cce34d3c465cc2d
AndranikMikhailyan <andranik.stav@mail.ru>
2018-11-19 19:08:32 +0300

    6.6. Двухмерный массив. Таблица умножения.[#84417]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,12 +3,10 @@ package ru.job4j.array;
 public class Check {
 
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 1; i < data.length; i++) {
             if (data[0] != data[i]) {
-                break;
-            } else {
-                result = true;
+                result = false;
             }
         }
         return result;
