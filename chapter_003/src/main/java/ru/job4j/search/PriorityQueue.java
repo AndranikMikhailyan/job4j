3ef819b1ce3668acce371c0d2b48e0b72770dd5e
AndranikMikhailyan <andranik.stav@mail.ru>
2018-12-09 21:14:22 +0300

    2. Очередь с приоритетом на LinkedList

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,9 +7,6 @@ public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-        if (this.tasks.isEmpty()) {
-            this.tasks.add(task);
-        }
         for (int i = 0; i < tasks.size(); i++) {
             if (tasks.get(i).getPriority() > task.getPriority()) {
                 tasks.add(i, task);
