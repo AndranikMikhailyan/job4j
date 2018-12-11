e564241ef01572a80d4ff4672f60c839ea906ce1
AndranikMikhailyan <andranik.stav@mail.ru>
2018-12-11 18:46:49 +0300

    Кофемашинка Исправил код

--- chapter_002/src/main/java/ru/job4j/chenger/CoffeMachine.java
+++ chapter_002/src/main/java/ru/job4j/chenger/CoffeMachine.java
@@ -1,50 +1,27 @@
 package ru.job4j.chenger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class CoffeMachine {
 
     int[] chenges(int value, int price) {
         int difference = value - price;
-        int count10 = 0;
-        int count5 = 0;
-        int count2 = 0;
-        int count1 = 0;
-        int arrayLenght = 0;
-        for (int i = difference; i >= 10; i -= 10) {
-            difference -= 10;
-            count10++;
-        }
-        for (int i = difference; i >= 5; i -= 5) {
-            difference -= 5;
-            count5++;
-        }
-        for (int i = difference; i >= 2; i -= 2) {
-            difference -= 2;
-            count2++;
-        }
-        for (int i = difference; i >= 1; i -= 1) {
-            difference -= 1;
-            count1++;
-        }
-        arrayLenght = count10 + count5 + count2 + count1;
-        int[] chenges = new int[arrayLenght];
-        if (count10 > 0) {
-            for (int i = 0; i < count10; i++) {
-                chenges[i] = 10;
+        int[] coins = new int[]{10, 5, 2, 1};
+        List<Integer> temp = new ArrayList<>();
+        for (Integer coin : coins) {
+            while (difference >= coin) {
+                difference -= coin;
+                temp.add(coin);
             }
         }
-        if (count5 > 0) {
-            for (int i = count10; i < count10 + count5; i++) {
-                chenges[i] = 5;
-            }
+        int[] result = new int[temp.size()];
+        int i = 0;
+        for (Integer integer : temp) {
+            result[i++] = integer;
         }
-        if (count2 > 0) {
-            for (int i = count10 + count5; i < count10 + count5 + count2; i++) {
-                chenges[i] = 2;
-            }
-        }
-        if (count1 > 0) {
-            chenges[chenges.length - 1] = 1;
-        }
-        return chenges;
+        return result;
     }
 }
