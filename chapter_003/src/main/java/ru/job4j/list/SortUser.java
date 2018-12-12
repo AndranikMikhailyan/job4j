bda48008c74d7c324bddf1899c4b097af2be382f
AndranikMikhailyan <andranik.stav@mail.ru>
2018-12-12 21:56:38 +0300

    1. Организовать сортировку User Исправил по требованиям задания

--- chapter_003/src/main/java/ru/job4j/list/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/list/SortUser.java
@@ -2,14 +2,8 @@ package ru.job4j.list;
 
 import java.util.*;
 
-public class SortUser  {
+public class SortUser {
     public Set<User> sort(List<User> list) {
-        list.sort(new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.compareTo(o2);
-            }
-        });
         TreeSet<User> userTreeSet = new TreeSet<>();
         for (User user : list) {
             userTreeSet.add(user);
@@ -17,3 +11,4 @@ public class SortUser  {
         return userTreeSet;
     }
 }
+
