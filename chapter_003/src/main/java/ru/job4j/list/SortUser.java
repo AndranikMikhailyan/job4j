9f1b6adcc0dcf0e8f54ccb4d168c9cd09a24d057
AndranikMikhailyan <andranik.stav@mail.ru>
2018-12-12 23:05:04 +0300

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/list/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/list/SortUser.java
@@ -6,5 +6,28 @@ public class SortUser {
     public Set<User> sort(List<User> list) {
         return new TreeSet<>(list);
     }
+
+    public List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return list;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return 0 != o1.getName().compareTo(o2.getName())
+                        ? o1.getName().compareTo(o2.getName()) : o1.compareTo(o2);
+            }
+        });
+        return list;
+    }
+
+
 }
 
--- chapter_003/src/test/java/ru/job4j/list/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/list/SortUserTest.java
@@ -28,4 +28,42 @@ public class SortUserTest {
             );
         assertThat(result, is(except));
      }
+
+    @Test
+    public void whenSortNameLength() {
+        User aleksandr = new User("Александр", 30);
+        User viktor = new User("Виктор", 30);
+        User anya = new User("Аня", 30);
+        User dasha = new User("Даша", 30);
+        List<User> users = new ArrayList<>();
+        users.add(aleksandr);
+        users.add(viktor);
+        users.add(anya);
+        users.add(dasha);
+        SortUser sortUser = new SortUser();
+        List<User> result = sortUser.sortNameLength(users);
+        List<User> except = new ArrayList<>(
+                Arrays.asList(anya, dasha, viktor, aleksandr)
+        );
+        assertThat(result, is(except));
+    }
+
+    @Test
+    public void whenSortByAllFields() {
+        User aleksandr1 = new User("Александр", 30);
+        User aleksandr2 = new User("Александр", 29);
+        User anya = new User("Аня", 25);
+        User dasha = new User("Даша", 30);
+        List<User> users = new ArrayList<>();
+        users.add(aleksandr1);
+        users.add(aleksandr2);
+        users.add(anya);
+        users.add(dasha);
+        SortUser sortUser = new SortUser();
+        List<User> result = sortUser.sortByAllFields(users);
+        List<User> except = new ArrayList<>(
+                Arrays.asList(aleksandr2, aleksandr1, anya, dasha)
+        );
+        assertThat(result, is(except));
+    }
 }
