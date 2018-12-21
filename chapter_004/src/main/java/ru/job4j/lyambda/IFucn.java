79781035d7b331f731e21342d34fd7664147fba8
AndranikMikhailyan <andranik.stav@mail.ru>
2018-12-22 00:11:00 +0300

    В классе StartUI заменить вывод в консоль на Consumer. Переделал.

--- chapter_004/src/main/java/ru/job4j/lyambda/IFucn.java
+++
@@ -1,8 +0,0 @@
-//package ru.job4j.lyambda;
-//
-//import java.util.List;
-//import java.util.function.Function;
-//
-//public interface IFucn {
-//    List<Double> diapasone(int start, int end, Function<Double, Double> func);
-//}
