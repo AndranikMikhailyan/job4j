37aa035f40c3c26b69d8255e2c2e5c2394701fdf
AndranikMikhailyan <andranik.stav@mail.ru>
2018-12-21 17:48:18 +0300

    Сортировка департаментов. Добавил javadoc

--- chapter_003/src/main/java/ru/job4j/department/Department.java
+++ chapter_003/src/main/java/ru/job4j/department/Department.java
@@ -2,14 +2,30 @@ package ru.job4j.department;
 
 import java.util.*;
 
+/**
+ * @author Andranik Mikhailyan.
+ * @version 0.1.
+ * @since 0.1.
+ */
 public class Department {
 
+    /**
+     * Переменная departments хранит список департаментов.
+     */
     private List<String> departments = new ArrayList<>();
 
+    /**
+     * Метод возвращает список департаментов.
+     * @return departments (Список департаментов).
+     */
     public List<String> getDepartments() {
         return departments;
     }
 
+    /**
+     * Метод добавляет новые департаменты в список.
+     * @param department Новый департамент.
+     */
     public void addDepartment(String department) {
         String[] split = department.split("\\\\");
         StringBuilder current = new StringBuilder();
@@ -22,6 +38,10 @@ public class Department {
         }
     }
 
+    /**
+     * Метод сортирует список департаментов в возрастающем порядке.
+     * @return departments (Отсортированный список департаментов).
+     */
     public List<String> ascendingSort() {
          Collections.sort(this.departments, new Comparator<String>() {
             @Override
@@ -32,6 +52,10 @@ public class Department {
          return departments;
     }
 
+    /**
+     * Метод сортирует список департаментов в убывающем порядке с сохранением иерархии департаментов.
+     * @return departments (Отсортированный список департаментов).
+     */
     public List<String> descendingSort() {
         Comparator<String> lengthComp = new Comparator<String>() {
             @Override
