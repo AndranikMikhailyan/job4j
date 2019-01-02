cc7f23253f3516d7f4d11ddf340c569a1cc9896a
AndranikMikhailyan <andranik.stav@mail.ru>
2019-01-02 21:05:28 +0300

    5.2.2. Реализовать Store<T exten

---
+++ junior_chapter_001/src/main/java/ru/job4j/generic/UserStore.java
@@ -0,0 +1,9 @@
+package ru.job4j.generic;
+
+public class UserStore extends BaseStore<User> {
+
+    public UserStore(int size) {
+        super(size);
+    }
+
+}
---
+++ junior_chapter_001/src/test/java/ru/job4j/generic/UserStoreTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.generic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class UserStoreTest {
+
+    @Test
+    public void whenAddObjectThenRoleStoreHasObject() {
+        UserStore userStore = new UserStore(10);
+        User user = new User("A0001");
+        userStore.add(user);
+        assertThat(userStore.findById("A0001"), is(user));
+    }
+
+    @Test
+    public void whenRepleceObjectThenTrue() {
+        UserStore userStore = new UserStore(10);
+        User user1 = new User("A0001");
+        User user2 = new User("A0002");
+        userStore.add(user1);
+        assertThat(userStore.replace("A0001", user2), is(true));
+    }
+
+    @Test
+    public void whenDeleteObjectThenTrue() {
+        UserStore userStore = new UserStore(10);
+        User user = new User("A0001");
+        userStore.add(user);
+        assertThat(userStore.delete("A0001"), is(true));
+    }
+
+    @Test
+    public void whenFindByIdThenReturnObject() {
+        UserStore userStore = new UserStore(10);
+        User user = new User("A0001");
+        userStore.add(user);
+        assertThat(userStore.findById("A0001"), is(user));
+    }
+
+    @Test
+    public void whenRoleStoreIsEmptyThenReturnNull() {
+        UserStore userStore = new UserStore(10);
+        User user = new User("A0001");
+        assertNull(userStore.findById("A0001"));
+    }
+}
