c0ea72c9b9bf82aa6f9b406b59a97af7e2bf7f4b
AndranikMikhailyan <andranik.stav@mail.ru>
2019-01-29 00:24:59 +0300

    4. Архивировать проект.

--- junior_chapter_001/src/main/java/ru/job4j/inpututput/io/Archiver.java
+++ junior_chapter_001/src/main/java/ru/job4j/inpututput/io/Archiver.java
@@ -1,28 +1,37 @@
 package ru.job4j.inpututput.io;
 
 import java.io.*;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Archiver {
 
-    public void toZip(String dir) throws IOException {
+    public void toZip(String dir, List<String> exts) throws IOException {
         ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(dir + ".zip"));
         File file = new File(dir);
         String prefix = file.getParent();
         if (file.isFile()) {
             String fileName = file.getPath().substring(prefix.length() + 1);
-            archive(file, zipOut, fileName);
+            exts.stream().forEach(s -> {
+                if (fileName.endsWith("." + s)) {
+                    try {
+                        archive(file, zipOut, fileName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         } else if (isEmptyDirectory(file)) {
             zipOut.putNextEntry(new ZipEntry(file.getName() + "/"));
             zipOut.closeEntry();
         } else {
-            toArchiveTheFile(file, zipOut, prefix);
+            toArchiveTheFile(file, zipOut, prefix, exts);
         }
         zipOut.close();
     }
 
-    private void toArchiveTheFile(File src, ZipOutputStream zipOut, String prefix) throws IOException {
+    private void toArchiveTheFile(File src, ZipOutputStream zipOut, String prefix, List<String> exts) throws IOException {
         String pref = prefix;
         for (File file : src.listFiles()) {
             String entryName = file.getPath().substring(pref.length() + 1);
@@ -31,10 +40,18 @@ public class Archiver {
                     zipOut.putNextEntry(new ZipEntry(entryName + "/"));
                     zipOut.closeEntry();
                 } else {
-                    toArchiveTheFile(file, zipOut, pref);
+                    toArchiveTheFile(file, zipOut, pref, exts);
                 }
             } else {
-                archive(file, zipOut, entryName);
+                exts.stream().forEach(s -> {
+                    if (entryName.endsWith("." + s)) {
+                        try {
+                            archive(file, zipOut, entryName);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         }
 
--- junior_chapter_001/src/test/java/ru/job4j/inpututput/io/ArchiverTest.java
+++ junior_chapter_001/src/test/java/ru/job4j/inpututput/io/ArchiverTest.java
@@ -4,11 +4,14 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArchiverTest {
 
     @Test
     public void when() throws IOException {
         Archiver archiver = new Archiver();
-        archiver.toZip("C:\\projects\\job4j");
+        archiver.toZip("C:\\projects\\job4j", List.of("class"));
     }
 }
