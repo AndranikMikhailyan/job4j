93a72ce8792a0a56b1df350dab8af970c955ff70
AndranikMikhailyan <andranik.stav@mail.ru>
2019-02-01 19:50:37 +0300

    4. Архивировать проект.

--- junior_chapter_001/src/test/java/ru/job4j/inpututput/io/ArchiverTest.java
+++ junior_chapter_001/src/test/java/ru/job4j/inpututput/io/ArchiverTest.java
@@ -10,22 +10,12 @@ import java.util.List;
 public class ArchiverTest {
 
     @Test
-    public void when() throws IOException {
-        Archiver archiver = new Archiver();
-        String suffix = "junior_chapter_001";
-        StringBuilder dir = new StringBuilder();
-        dir.append(System.getProperty("user.dir"));
-        dir.delete(dir.toString().length() - 1 - suffix.length(), dir.toString().length());
-        archiver.toZip(dir.toString(), List.of("class"));
-    }
-
-    @Test
     public void when2() throws IOException {
         Archiver2 archiver2 = new Archiver2();
         String suffix = "junior_chapter_001";
         StringBuilder dir = new StringBuilder();
         dir.append(System.getProperty("user.dir"));
         dir.delete(dir.toString().length() - 1 - suffix.length(), dir.toString().length());
-        archiver2.toZip(dir.toString(), List.of("class", "java", "xml"));
+        archiver2.toZip(dir.toString(), "temp.zip", List.of("class", "java", "xml"));
     }
 }
