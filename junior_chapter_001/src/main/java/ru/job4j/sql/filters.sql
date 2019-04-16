c1de8c89af80ed455c4438c826615f70ff86018c
AndranikMikhailyan <andranik.stav@mail.ru>
2019-04-16 22:28:18 +0300

    Фильтры. Исправление замечаний.

--- junior_chapter_001/src/main/java/ru/job4j/sql/filters.sql
+++ junior_chapter_001/src/main/java/ru/job4j/sql/filters.sql
@@ -4,9 +4,9 @@ select * from product as p where p.name like '%мороженное%';
 
 select * from product as p where extract(month from p.expired_date)=extract(month from current_date)+1;
 
-select max(price) from product;
+select product.name from product having max(price);
 
-select count(*)from product as p inner join type as t on p.type_id=t.id group by t.type;
+select t.type, count(*)from product as p inner join type as t on p.type_id=t.id group by t.type;
 
 select * from product as p inner join type as t on p.type_id=t.id where t.type = 'СЫР' or t.type='МОЛОКО';
 
