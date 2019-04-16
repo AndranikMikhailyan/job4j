e2b8f18682474e1741fc8614ee28393060072b91
AndranikMikhailyan <andranik.stav@mail.ru>
2019-04-16 20:35:16 +0300

    Фильтры

---
+++ junior_chapter_001/src/main/java/ru/job4j/sql/filters.sql
@@ -0,0 +1,15 @@
+select * from product as p inner join type as t on p.type_id=t.id where type = 'СЫР';
+
+select * from product as p where p.name like '%мороженное%';
+
+select * from product as p where extract(month from p.expired_date)=extract(month from current_date)+1;
+
+select max(price) from product;
+
+select count(*)from product as p inner join type as t on p.type_id=t.id group by t.type;
+
+select * from product as p inner join type as t on p.type_id=t.id where t.type = 'СЫР' or t.type='МОЛОКО';
+
+select t.type from product as p inner join type as t on p.type_id=t.id group by t.type having count(*)<10;
+
+select * from product as p inner join type as t on p.type_id=t.id;
